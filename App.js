acc4e6d64aac33e7d2178bb0c2240efb5ba937b3
gk90731 <gk90731@gmail.com>
2020-07-14 20:10:13 +0530

    basic setup commit

--- App.js
+++ App.js
@@ -1,21 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, ToolbarAndroid } from 'react-native';
+import NavigationBar from 'react-native-navbar';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+const titleConfig = {
+  title: 'Hello, world',
+};
+const rightButtonConfig = {
+  title: 'Next',
+  handler: () => alert('hello!'),
+};
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  
+  render() { 
+    return ( 
+      <View style={styles.container}>
+      {/* <Text>Helo Worljgh</Text> */}
       <StatusBar style="auto" />
-    </View>
-  );
+      <NavigationBar
+        title={titleConfig}
+        rightButton={rightButtonConfig}
+      />
+      </View>
+     );
+  }
 }
+ 
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor:"dodgerblue"
   },
 });
