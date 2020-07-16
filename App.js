f24fed6ccd37ff4c87b97a273b1eb47e346594b1
gk90731 <gk90731@gmail.com>
2020-07-16 11:16:59 +0530

    rn-pdf-reader working with remote pdf url

--- App.js
+++ App.js
@@ -1,41 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import React, {Component} from 'react';
-import { StyleSheet, Text, View, ToolbarAndroid } from 'react-native';
+import * as React from 'react'
+import { StyleSheet, Text, View, SafeAreaView} from 'react-native';
+import PDFReader from 'rn-pdf-reader-js'
 import NavigationBar from 'react-native-navbar';
 
+
 const titleConfig = {
-  title: 'Hello, world',
+  title: 'PDF Reader',
+  tintColor : 'white'
 };
 const rightButtonConfig = {
   title: 'Next',
   handler: () => alert('hello!'),
+  tintColor : 'white'
 };
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {  }
-  }
-  
-  render() { 
-    return ( 
-      <View style={styles.container}>
-      {/* <Text>Helo Worljgh</Text> */}
-      <StatusBar style="auto" />
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={{flex:1,height:70}}>
+      <View>
       <NavigationBar
+        style={styles.navigation}
         title={titleConfig}
         rightButton={rightButtonConfig}
       />
       </View>
-     );
+      <PDFReader
+        source={{
+          uri: 'http://samples.leanpub.com/thereactnativebook-sample.pdf',
+        }}
+      />
+      </View>
+    )
   }
 }
- 
-export default App;
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor:"dodgerblue"
+  navigation: {
+    backgroundColor:"dodgerblue",
+    paddingRight:25
   },
+  statusBar: {
+    
+  }
 });
