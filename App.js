b1a390f03c03161a659935e9c8745f1a6ae3c1bc
gk90731 <gk90731@gmail.com>
2020-07-16 21:47:20 +0530

    webview working

--- App.js
+++ App.js
@@ -1,7 +1,13 @@
-import * as React from 'react'
+import { StatusBar } from 'expo-status-bar';
+import React, {Component} from 'react';
 import { StyleSheet, Text, View, SafeAreaView} from 'react-native';
-import PDFReader from 'rn-pdf-reader-js'
+import {WebView} from 'react-native-webview';
+import { Constants } from 'expo';
 import NavigationBar from 'react-native-navbar';
+// import PDFView from 'react-native-view-pdf/lib/index';
+// import PDFView from 'react-native-view-pdf';
+// import Pdf from 'react-native-pdf';
+// import PDFReader from 'rn-pdf-reader-js'
 
 
 const titleConfig = {
@@ -13,30 +19,39 @@ const rightButtonConfig = {
   handler: () => alert('hello!'),
   tintColor : 'white'
 };
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={{flex:1,height:70}}>
-      <View>
-      <NavigationBar
-        style={styles.navigation}
-        title={titleConfig}
-        rightButton={rightButtonConfig}
-      />
+const resources = {
+  file: Platform.OS === 'ios' ? 'test-pdf.pdf' : '/sdcard/Download/test-pdf.pdf',
+  url: 'https://www.ets.org/Media/Tests/TOEFL/pdf/SampleQuestions.pdf',
+  base64: 'JVBERi0xLjMKJcfs...',
+};
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  
+  render() { 
+    let yourPDFURI = {uri:'http://samples.leanpub.com/thereactnativebook-sample.pdf', cache: true};
+    const resourceType = 'url';
+    return ( 
+    <View style={{flex:1,paddingTop: 25}}>
+        <WebView
+          bounces={false}
+          scrollEnabled={false} 
+          source={{ uri: 'https://www.ets.org/Media/Tests/TOEFL/pdf/SampleQuestions.pdf' }} />
       </View>
-      <PDFReader
-        source={{
-          uri: 'http://samples.leanpub.com/thereactnativebook-sample.pdf',
-        }}
-      />
-      </View>
-    )
+     );
   }
 }
+ 
+export default App;
+
 const styles = StyleSheet.create({
   navigation: {
     backgroundColor:"dodgerblue",
-    paddingRight:25
+    paddingRight:25,
+    marginTop:20
   },
   statusBar: {
     
